f26176399739001e7a3409d80ea7672be3addd37
Memfagor <memfagor@gmail.com>
2018-11-16 15:47:10 +0100

    Add board analyzing functions

--- tictactoe.py
+++ tictactoe.py
@@ -2,8 +2,9 @@
 
 class TicTacToe():
 
-    def __init__(self, board_size):
+    def __init__(self, board_size=3):
         self.bs = board_size
+        self.ba = []
         self.board = [[' ' for y in range(self.bs)] for x in range(self.bs)]
 
     def getBoardSize(self):
@@ -26,16 +27,110 @@ class TicTacToe():
         return self.board[0][y]
 
     def checkDiagFall(self):
-        for x in range(1, self.bs):
-            if self.board[x][x] != self.board[0][0]: break
+        for i in range(1, self.bs):
+            if self.board[i][i] != self.board[0][0]: break
         else: return self.board[0][0]
         return False
 
     def checkDiagRise(self):
-        for x in range(1, self.bs):
-            if self.board[self.bs - 1 -x][x] != self.board[self.bs - 1][0]:
+        for y in range(1, self.bs):
+            x = self.bs -1 - y
+            if self.board[x][y] != self.board[x][0]:
                 break
-            else: return self.board[self.bs - 1][0]
+            else: return self.board[x][0]
             return False
 
+    def boardAnalyzeHoriz(self, mark='x'):
+        analyze = []
+        for x in range(self.bs):
+            l = {}
+            l['empty_list'] = []
+            l['mine_list'] = []
+            l['opponent_list'] = []
+            for y in range(self.bs):
+                if self.board[x][y] == ' ':
+                    l['empty'] = l.get('empty', 0) + 1
+                    l['empty_list'].append((x, y))
+                elif self.board[x][y] == mark:
+                    l['mine'] = l.get('mine', 0) + 1
+                    l['mine_list'].append((x, y))
+                else:
+                    l['opponent'] = l.get('opponent', 0) + 1
+                    l['opponent_list'].append((x, y))
+            if l['empty'] != 0 and l['mine'] = 0 or l['opponent'] = 0:
+                analyze.append(l)
+        return analyze
 
+    def boardAnalyzeVert(self, mark='x'):
+        analyze = []
+        for y in range(self.bs):
+            l = {}
+            l['empty_list'] = []
+            l['mine_list'] = []
+            l['opponent_list'] = []
+            for x in range(self.bs):
+                if self.board[x][y] == ' ':
+                    l['empty'] = l.get('empty', 0) + 1
+                    l['empty_list'].append((x, y))
+                elif self.board[x][y] == mark:
+                    l['mine'] = l.get('mine', 0) + 1
+                    l['mine_list'].append((x, y))
+                else:
+                    l['opponent'] = l.get('opponent', 0) + 1
+                    l['opponent_list'].append((x, y))
+            if l['empty'] != 0 and l['mine'] = 0 or l['opponent'] = 0:
+                analyze.append(l)
+        return analyze
+
+    def boardAnalyzeDiagFall(self, mark='x'):
+        analyze = []
+        l = {}
+        l['empty_list'] = []
+        l['mine_list'] = []
+        l['opponent_list'] = []
+        for i in range(self.bs):
+            if self.board[i][i] == ' ':
+                l['empty'] = l.get('empty', 0) + 1
+                l['empty_list'].append((i, i))
+            elif self.board[i][i] == mark:
+                l['mine'] = l.get('mine', 0) + 1
+                l['mine_list'].append((i, i))
+            else:
+                l['opponent'] = l.get('opponent', 0) + 1
+                l['opponent_list'].append((i, i))
+        if l['empty'] != 0 and l['mine'] = 0 or l['opponent'] = 0:
+            return analyze.append(l)
+        else:
+            return analyze
+
+    def boardAnalyzeDiagRise(self, mark='x'):
+        analyze = []
+        l = {}
+        l['empty_list'] = []
+        l['mine_list'] = []
+        l['opponent_list'] = []
+        for y in range(self.bs):
+            x = self.bs - 1 - y
+            if self.board[x][y] == ' ':
+                l['empty'] = l.get('empty', 0) + 1
+                l['empty_list'].append((x, y))
+            elif self.board[x][y] == mark:
+                l['mine'] = l.get('mine', 0) + 1
+                l['mine_list'].append((x, y))
+            else:
+                l['opponent'] = l.get('opponent', 0) + 1
+                l['opponent_list'].append((x, y))
+        if l['empty'] != 0 and l['mine'] = 0 or l['opponent'] = 0:
+            return analyze.append(l)
+        else:
+            return analyze
+
+
+    def boardAnalyze(self, mark='x'):
+        self.ba = []
+        self.ba.extend(self.boardAnalyzeHoriz(mark))
+        self.ba.extend(self.boardAnalyzeVert(mark))
+        self.ba.extend(self.boardAnalyzeDiagRise(mark))
+        self.ba.extend(self.boardAnalyzeDiagFall(mark))
+
+        
