92faaf2a9744abc5cb1967aa4bb08ffc6813b500
Memfagor <memfagor@gmail.com>
2018-11-20 14:51:19 +0100

    Merge the line analyzing functions into new lineDict subclass

--- tictactoe.py
+++ tictactoe.py
@@ -2,6 +2,35 @@
 
 class TicTacToe():
 
+    class lineDict():
+
+        def __init__(self):
+            self. d = {}
+            self.d['empty'] = 0
+            self.d['empty_list'] = []
+            self.d['mine'] = 0
+            self.d['mine_list'] = []
+            self.d['opponent'] = 0
+            self.d['opponent_list'] = []
+
+        def __call__(self):
+            return self.d
+
+        def importance(self):
+            return self.d['empty'] != 0 and self.d['mine'] == 0 or self.d['opponent'] == 0
+
+        def fill(self, board, pos_x, pos_y):
+            if board[pos_x][pos_y] == ' ':
+                self.d['empty'] = self.d.get('empty', 0) + 1
+                self.d['empty_list'].append((pos_x, pos_y))
+            elif board[pos_x][pos_y] == mark:
+                self.d['mine'] = self.d.get('mine', 0) + 1
+                self.d['mine_list'].append((pos_x, pos_y))
+            else:
+                self.d['opponent'] = self.d.get('opponent', 0) + 1
+                self.d['opponent_list'].append((pos_x, pos_y))
+
+
     def __init__(self, board_size=3):
         self.bs = board_size
         self.ba = []
@@ -53,67 +82,43 @@ class TicTacToe():
             else: return self.board[x][0]
             return False
 
-    def generateAnalyzeDict():
-        d = {}
-        d['empty'] = 0
-        d['empty_list'] = []
-        d['mine'] = 0
-        d['mine_list'] = []
-        d['opponent'] = 0
-        d['opponent_list'] = []
-        return d
-
-    def lineImportance(d):
-        return d['empty'] != 0 and d['mine'] == 0 or d['opponent'] == 0
-
-    def fillDict(b, d, pos_x, pos_y):
-        if b[pos_x][pos_y] == ' ':
-            d['empty'] = d.get('empty', 0) + 1
-            d['empty_list'].append((pos_x, pos_y))
-        elif b[pos_x][pos_y] == mark:
-            d['mine'] = d.get('mine', 0) + 1
-            d['mine_list'].append((pos_x, pos_y))
-        else:
-            d['opponent'] = d.get('opponent', 0) + 1
-            d['opponent_list'].append((pos_x, pos_y))
-
     def boardAnalyzeHoriz(self, mark='x'):
         analyze = []
         for x in range(self.bs):
-            l = generateAnalyzeDict()
+            l = TicTacToe.lineDict()
             for y in range(self.bs):
-                fillDict(self.board, l, x, y)
-            if lineImportance(l):
-                analyze.append(l)
+                l.fill(self.board, x, y)
+            if l.importance():
+                analyze.append(l())
         return analyze
 
     def boardAnalyzeVert(self, mark='x'):
         analyze = []
         for y in range(self.bs):
-            l = generateAnalyzeDict()
+            l = TicTacToe.lineDict()
             for x in range(self.bs):
-                fillDict(self.board, l, x, y)
-            if lineImportance(l):
-                analyze.append(l)
+                l.fill(self.board, x, y)
+            if l.importance():
+                analyze.append(l())
         return analyze
 
     def boardAnalyzeDiagFall(self, mark='x'):
         analyze = []
-        l = generateAnalyzeDict()
+        l = TicTacToe.lineDict()
         for i in range(self.bs):
-            fillDict(self.board, l, i, i)
-        if lineImportance(l):
-            analyze.append(l)
+            l.fill(self.board, i, i)
+        if l.importance():
+            analyze.append(l())
         return analyze
 
     def boardAnalyzeDiagRise(self, mark='x'):
         analyze = []
-        l = generateAnalyzeDict()
+        l = TicTacToe.lineDict()
         for y in range(self.bs):
             x = self.bs - 1 - y
-            fillDict(self.board, l, x, y)
-        if lineImportance(l):
-            analyze.append(l)
+            l. fill(self.board, x, y)
+        if l.importance():
+            analyze.append(l())
         return analyze
 
 
